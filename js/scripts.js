cc076732904f824c8425a5eab5589185dc97e9e2
Christopher Gosser and Liza Tennis <christopher@>
2018-08-06 13:41:44 -0700

    add multiply, divide, subract and add functions to the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-function poodle(age,name,food){
-age = prompt("what is your age?");
-name = prompt("what is your name?");
-food = prompt("what is your favorite food?");
-alert("youre name is " + name + " and you are " + age + " and your favorite food is " + food);
+var add = function(number1, number2){
+  return number1 + number2;
 };
+
+var subtract = function(number1, number2){
+  return number1 - number2;
+};
+
+var multipy = function(number1, number2){
+  return number1 * number2;
+};
+
+var divide = function(number1, number2){
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Give me a number"));
+var number2 = parseInt(prompt("Give me another number"));
+var result = ("Those two numbers added together give you " + add(number1, number2));
+alert(result);
