2cc0e1d77dc42f0a8b6c3e9f431cf2c1da2c949b
Christopher Gosser and Liza Tennis <liza@>
2018-08-06 13:06:24 -0700

    create js folder with scripts file and add a readme

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function poodle(age,name,food){
+age = prompt("what is your age?");
+name = prompt("what is your name?");
+food = prompt("what is your favorite food?");
+alert("youre name is " + name + " and you are " + age + " and your favorite food is " + food);
+};
