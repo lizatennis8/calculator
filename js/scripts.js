c5a2c1db3dcdc79dd98df858f14dfbd8a1667a83
Christopher Gosser and Liza Tennis <liza@>
2018-08-06 16:37:37 -0700

    add jquery starters

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,5 @@
-var add = function(number1, number2){
-  return number1 + number2;
-};
-
-var subtract = function(number1, number2){
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2){
-  return number1 * number2;
-};
-
-var divide = function(number1, number2){
-  return number1 / number2;
-};
-
-var bmi = function(weight, height){
-  return (weight / height) / height;
-};
-
-var temperatureCtoF= function(celcius){
-  return celcius * 1.8 +32;
-};
-
-var celcius = parseInt(prompt("Temp in celcius"));
-alert("The temp in Fahrenheight is " + temperatureCtoF(celcius));
+$(document).ready(function() {
+  $ ("h1").click(function() {
+    alert("This is a header.");
+  });
+});
